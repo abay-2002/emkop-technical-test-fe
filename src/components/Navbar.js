714acb6b0346e00431d831c6e248e6e92dc15898
abay-2002 <akbar121202@gmail.com>
2023-02-18 08:48:13 +0800

    added search functionallity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg bg-primary px-2">
                 <div className="container-fluid">
-                    <Link className="navbar-brand text-white fw-semibold" to="/">Navbar</Link>
+                    <Link className="navbar-brand text-white fw-semibold" to="/">Emkop | Technical test</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- src/pages/ReadDeleteUpdateSearch.js
+++ src/pages/ReadDeleteUpdateSearch.js
@@ -33,18 +33,15 @@ export default function ReadOrDelete() {
     const { isFetching, people } = useRead();
     
     // Delete
-    function handleDelete(e, userId){
+    function handleDelete(e){
         e.preventDefault()
         setIsDeleting(true);
-        fetch(`${process.env.REACT_APP_PROXY}/api/karyawan/delete/${userId}`, {
+        fetch(`${process.env.REACT_APP_PROXY}/api/karyawan/delete`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                userId
-            })
         }).then(res => {
             return res.json()
         }).then(response => {
@@ -60,6 +57,38 @@ export default function ReadOrDelete() {
     function updateHandler(e){
         e.preventDefault()
         setIsUpdating(true)
+    }
+
+    // search
+    const [searchId, setSearchId] = useState(null);
+    
+    const [ searchData, setSearchData ] = useState(null);
+    const [ isSearching, setIsSearching ] = useState(false);
+    
+    function getVal(e){
+        setSearchId(parseInt(e.target.value))
+        setSearchData(null)
+    }
+   
+    function searchHandler(e, searchId){
+        setIsSearching(true);
+        console.log(searchId)
+
+        
+        e.preventDefault();
+        fetch(`${process.env.REACT_APP_PROXY}/api/karyawan/search/${searchId}`, {
+            method: 'GET',
+        })
+        .then(res => {
+            return res.json()
+        }).then(response => {
+            setIsSearching(false);
+            setSearchData(response)
+            console.log(response)
+        }).catch(err => {
+            console.log(err)
+        })
+        setSearchId(null)
     }
 
     return (
@@ -80,67 +109,112 @@ export default function ReadOrDelete() {
             {/* Table rows exists */}
             {people.length !== 0 &&
                 <div>
-                    <label htmlFor="search" className="mb-1 fw-semibold">Search</label>
+                    {!isSearching && 
                     <div className="mb-3">
-                        <form>
-                            <input type="text" id="search" className="form-control mb-2" aria-describedby="passwordHelpBlock" placeholder="Akbar Angkasa" />
+                        <form onSubmit={(e) => searchHandler(e, searchId)}>
+                            <label htmlFor="search" className="mb-1 fw-semibold">Search user by id</label>
+                            <input type="number" onChangeCapture={(e) => getVal(e)} id="search" name="userId" className="form-control mb-2" aria-describedby="passwordHelpBlock" placeholder="12" required/>
                             <button type="submit" className="btn-sm fw-semibold btn btn-primary"><i className="bi bi-search"></i></button>
                         </form>
                     </div>
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Dob</th>
-                                <th scope="col">Gender</th>
-                                <th scope="col">Department</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {people.map((person, i) => (
-                                <tr key={i}>
-                                    {isDeleting &&
-                                    <td>
-                                        <div className="spinner-border text-primary" role="status">
-                                            <span className="visually-hidden">Loading...</span>
-                                        </div>
-                                    </td>
-                                    }
-                                    {isUpdating &&
-                                    <td>
-                                        <div className="spinner-border text-primary" role="status">
-                                            <span className="visually-hidden">Loading...</span>
-                                        </div>
-                                    </td>
-                                    }
-                                    <th scope="row">{person.id}</th>
-                                    <td className="fw-semibold">{person.name}</td>
-                                    <td>{moment(person.dob).format('DD MMMM YYYY')}</td>
-                                    <td>{person.gender}</td>
-                                    <td>{person.department}</td>
-                                    <td className="d-flex flex-row">
-                                        {/* Update button */}
-                                        <div className="me-2">
-                                            <button type="button" className="btn btn-primary btn-sm fw-semibold" data-bs-toggle="modal" data-bs-target={`#${person.gender+person.name.replace(' ','')+person.id}`}>
-                                                Update
-                                            </button>
-                                        </div>
-                                        {/* Delete btn form */}
-                                        <div className="me-2">
-                                            <form onSubmit={(e) => handleDelete(e, person.id)}>
-                                                <button type="submit" className="btn btn-danger btn-sm fw-semibold">
-                                                    Delete
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                </tr>
-                            ))
+                    }
+                    {isSearching &&
+                        <div className="spinner-border text-primary" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    }
+                    {/* Search result */}
+                    {searchData && 
+                        <div>
+                            <h2>Search result</h2>
+                            {searchData.length !== 0 &&
+                                <table className="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Dob</th>
+                                            <th scope="col">Gender</th>
+                                            <th scope="col">Department</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {searchData.map((data,i) => (
+                                            <tr key={i}>
+                                                <td>{data.id}</td>
+                                                <td>{data.name}</td>
+                                                <td>{data.dob}</td>
+                                                <td>{data.gender}</td>
+                                                <td>{data.department}</td>
+                                            </tr>
+                                        ))
+                                        }
+                                    </tbody>
+                                </table>
                             }
-                        </tbody>
-                    </table>
+                            {searchData.length === 0 &&
+                                <p>No result found</p>
+                            }
+                            <hr />
+                        </div>
+                    }
+                    <div>
+                        <h2>Karyawan</h2>
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Dob</th>
+                                    <th scope="col">Gender</th>
+                                    <th scope="col">Department</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {people.map((person, i) => (
+                                    <tr key={i}>
+                                        {isDeleting &&
+                                        <td>
+                                            <div className="spinner-border text-primary" role="status">
+                                                <span className="visually-hidden">Loading...</span>
+                                            </div>
+                                        </td>
+                                        }
+                                        {isUpdating &&
+                                        <td>
+                                            <div className="spinner-border text-primary" role="status">
+                                                <span className="visually-hidden">Loading...</span>
+                                            </div>
+                                        </td>
+                                        }
+                                        <th scope="row">{person.id}</th>
+                                        <td className="fw-semibold">{person.name}</td>
+                                        <td>{moment(person.dob).format('DD MMMM YYYY')}</td>
+                                        <td>{person.gender}</td>
+                                        <td>{person.department}</td>
+                                        <td className="d-flex flex-row">
+                                            {/* Update button */}
+                                            <div className="me-2">
+                                                <button type="button" className="btn btn-primary btn-sm fw-semibold" data-bs-toggle="modal" data-bs-target={`#${person.gender+person.name.replace(' ','')+person.id}`}>
+                                                    Update
+                                                </button>
+                                            </div>
+                                            {/* Delete btn form */}
+                                            <div className="me-2">
+                                                <form onSubmit={(e) => handleDelete(e, person.id)}>
+                                                    <button type="submit" className="btn btn-danger btn-sm fw-semibold">
+                                                        Delete
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                     {/* Start of Modals */}
                     {people.map((person, i) => (
                         <div key={i} className="modal fade" id={`${person.gender+person.name.replace(' ','')+person.id}`} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby={`${person.gender+person.name.replace(' ','')+person.id}Label`}aria-hidden="true">
